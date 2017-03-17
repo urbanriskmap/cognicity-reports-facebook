dc6eb8a4d9d0ac9301010886c593c954f4c4d86d
MaanasaPriyaa <maanasa@mit.edu>
2017-03-17 01:30:25 -0400

    Serverless Lambdas to get card OTL from server and send it to user via Facebook Messenger

---
+++ README.md
@@ -0,0 +1,38 @@
+# cognicity-reports-facebook
+This deploys serverless lambdas that, after user initiates a conversation, fetch a report card from the server and sends it to them via Facebook messenger
+
+### Install
+`npm install`
+
+### Getting started
+* Create Facebook app and page and get `APPSECRET` and `PAGEACCESSTOKEN` as explained [here](https://developers.facebook.com/docs/messenger-platform/guides/setup). Hold off on completing Step 2 (Set up webhooks) in the guide.
+* Add code for webhooks and add it to the functions in serverless.yml file as explained [here](https://serverless.com/blog/building-a-facebook-messenger-chatbot-with-serverless/)
+* Add the lambda function to listen to relevant SNS topics and add it to the functions in serverless.yml file
+* Set up the config files as explained in the `Configuration` section. `VALIDATIONTOKEN` can be set to any arbitrary string.
+* Now, deploy your serverless lambdas as mentioned in the `Run` section
+* On successful deployment, you'll get a secure URL for the webhook's GET method. Now complete Step 2 in the [Quick Start Guide](https://developers.facebook.com/docs/messenger-platform/guides/setup). Set this secure URL as 'Callback URL' and `VALIDATIONTOKEN` in the 'Verify Token' fields. Select 'messages' and 'messaging_postbacks' to enable two-way communication. Verifying and saving this enables the webhooks for the app.
+* Subscribe your app to your page as explained [here](https://developers.facebook.com/docs/messenger-platform/guides/setup#subscribe_app)
+* Send a text to your Facebook messenger bot to test if it is up and running!
+* Read `Misc Notes` section to assist in configuration
+
+### Run
+`serverless deploy`
+
+### Configuration
+Save a copy of sample.env as .env in local directory with appropriate credentials
+
+* `VALIDATIONTOKEN`: Give the same token on Facebook dev portal to validate webhooks
+* `APPSECRET`: AppSecret key got on creating facebook app
+* `PAGEACCESSTOKEN`: Access token for the page to which the app has subscribed
+* `DEFAULT_LANG`: Current default language is English. You can add more languages here and parameterize replies for each language.
+* `CARD_PATH`: Front end's cards URL
+* `MAPSERVER`: Front end's map URL
+* `X_API_KEY`: API Key needed to make calls to the deployed server (Set it to "" during local testing)
+* `PG_CON`: Connection string for the Postgres database
+* `SERVER`: Cognicity server URL to fetch unique cardIds
+
+#### Misc Notes
+- AWS credentials are stored in bash_profile
+- Grasp "username" is userID/senderID from source networks to allow replies in conversation
+- Errors are logged to console, but not returned to user currently
+- If you want to test with your local Cognicity server instance, set up secure tunnels to localhost using ngrok and use that URL in the .env file. Install 'npm install -g ngrok'. After initializing the server, run 'ngrok http <PORT_NUMBER'. Use the https URL generated and set it in the 'SERVER' section of the env file. This allows the Lambda to interact with the server to fetch card OTL.
---
+++ handler.js
@@ -0,0 +1,138 @@
+'use strict';
+
+const request = require('request');
+require('dotenv').config({silent:true});
+
+// GRASP card
+const options = {
+  host: process.env.SERVER,
+  path: '/cards',
+  method: 'POST',
+  port: 80,
+  headers: {
+    'x-api-key': process.env.X_API_KEY,
+    'Content-Type': 'application/json'
+  }
+}
+
+// GRASP operating regions
+const instance_regions = {
+  chn: 'chennai'
+}
+
+// Replies to user
+const replies = {
+  'en': 'Hi! Report using this link, thanks.'
+}
+
+// Confirmation message to user
+const confirmations = {
+  'en': "Hi! Thanks for your report. I've put it on the map."
+}
+
+/*
+ * Call the Send API. The message data goes in the body. If successful, we'll
+ * get the message id in a response
+ *
+ */
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: process.env.PAGEACCESSTOKEN },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s",
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+}
+
+// Webhook handler - This is the method called by Facebook when you verify webhooks for the app
+module.exports.webhook = (event, context, callback) => {
+  if (event.method === 'GET') {
+    // Facebook app verification
+    if (event.query['hub.verify_token'] === process.env.VALIDATIONTOKEN && event.query['hub.challenge']) {
+      return callback(null, parseInt(event.query['hub.challenge']));
+    } else {
+      return callback('Invalid token');
+    }
+  }
+
+  if (event.method === 'POST') {
+    event.body.entry.map((entry) => {
+      entry.messaging.map((messagingItem) => {
+        if (messagingItem.message && messagingItem.message.text) {
+          // Form JSON request body
+          var card_request = {
+            "username": messagingItem.sender.id.toString(),
+            "network": "facebook",
+            "language": process.env.DEFAULT_LANG
+          }
+
+          // Get a card from Cognicity server
+          request({
+            url: options.host + options.path,
+            method: options.method,
+            headers: options.headers,
+            port: options.port,
+            json: true,
+            body: card_request
+          }, function(error, response, body){
+            if (!error && response.statusCode === 200){
+              //Construct the text message to be sent to the user
+              var messageText = replies[process.env.DEFAULT_LANG];
+              messageText += "\n" + process.env.CARD_PATH + "flood" + "/" + body.cardId + "/report";
+              const payload = {
+                recipient: {
+                  id: messagingItem.sender.id
+                },
+                message: {
+                  text: messageText
+                }
+              };
+
+              callSendAPI(payload);
+            } else {
+              var err = 'Error getting card: ' + JSON.stringify(error) + JSON.stringify(response);
+              callback(err, null); // Return error
+            }
+          });
+        }
+      });
+    });
+  }
+};
+
+module.exports.reply = (event, context, callback) => {
+  //This module listens in to SNS Facebook topic and reads the message published
+  var message = JSON.parse(event.Records[0].Sns.Message);
+  console.log("Message received from SNS topic: " + message);
+
+  //Construct the confirmation message to be sent to the user
+  var messageText = confirmations[message.language];
+  messageText += "\n" + process.env.MAPSERVER + instance_regions[message.implementation_area] + '/' + message.report_id;
+  const payload = {
+    recipient: {
+      id: message.username
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  //Call Send API to confirmation message with report link to the user
+  callSendAPI(payload);
+};
