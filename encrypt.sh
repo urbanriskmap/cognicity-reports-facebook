43e02bdbaa2bee3ad48f7864ba3d348d38f32481
abrahamq <abrahamquintero2323@gmail.com>
2017-12-01 17:18:03 -0500

    deploy to production as well

--- encrypt.sh
+++ encrypt.sh
@@ -3,3 +3,4 @@
 #will encrypt env.yml into env.yml.enc
 #environment variables $fb_telegram_param_iv and $fb_telegram_param_key must be set correctly
 openssl enc -aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in Indonesia-dev.env.yml -out Indonesia-dev.env.yml.enc
+openssl enc -aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in Indonesia-master.env.yml -out Indonesia-master.env.yml.enc
