68a599385ec6e160256ddac2113a4878c53616d3
abrahamq <abrahamquintero2323@gmail.com>
2017-12-01 16:37:09 -0500

    typo

--- decrypt.sh
+++ decrypt.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-openssl aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in Indonesia-dev.env.yml.enc -out Indonesia.env.yml -d
+openssl aes-256-cbc -K $fb_telegram_param_key -iv $fb_telegram_param_iv -in Indonesia-dev.env.yml.enc -out Indonesia-dev.env.yml -d
