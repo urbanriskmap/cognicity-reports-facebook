e4b47c19328c0e941acc380847581844b0679ea0
Cory Johnson <corymj@mit.edu>
2017-12-22 16:31:18 -0600

    language updates

--- handler.js
+++ handler.js
@@ -27,22 +27,19 @@ const instance_regions = {
 // Welcome message to user (telegram)
 const initiate = {
   'en': 'Welcome! Type in /flood to request a card link',
-  'id': 'Selamat datang! ketik /banjir untuk meminta link kartu',
-  'in': 'Hai!!  Saya BencanaBot. Tekan menu di bawah atau ketik ’Laporkan banjir'
+  'id': 'Hai!!  Saya BencanaBot. Tekan menu di bawah atau ketik ’Laporkan banjir'
 }
 
 // Replies to user
 const replies = {
   'en': 'Hi! Report using this link, thanks.',
   'id': 'Hi! Laporkan menggunakan link ini. Terima kasih.',
-  'in': 'Hi! Laporkan menggunakan link ini. Terima kasih.'
 };
 
 // Confirmation message to user
 const confirmations = {
   'en': "Hi! Thanks for your report. I've put it on the map.",
   'id': 'Hi! Terima kasih atas laporan Anda. Aku sudah menaruhnya di peta.',
-  'in': 'Hi! Terima kasih atas laporan Anda. Aku sudah menaruhnya di peta.'
 };
 
 /*
