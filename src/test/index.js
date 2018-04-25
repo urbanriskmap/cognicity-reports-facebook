2ae04ddc12a2379a6df880ce92cee6b72366b1c8
Tomas <tomasholderness@gmail.com>
2018-04-25 16:34:28 -0400

    fixed testing for refactor

--- src/test/index.js
+++ src/test/index.js
@@ -6,30 +6,6 @@
  */
 
 // Unit tests
-import testCards from './testLibCards';
-import testFacebook from './testLibFacebook';
-import testMessages from './testMessages';
-import testReceive from './testReceive';
+import testFacebook from './testFacebook';
 
-const config = {
-  oauth: {
-    validation_token: process.env.FACEBOOK_VALIDATION_TOKEN,
-    page_access_token: 'riskmap-token',
-  },
-  app: {
-    default_lang: 'en',
-    default_locale: 'US',
-    facebook_endpoint: 'http://127.0.0.1',
-    map_endpoint: 'https://riskmap.us/map/',
-  },
-  server: {
-    card_endpoint: 'https://cards.riskmap.us/flood/',
-    card_api: 'http://127.0.0.1',
-    api_key: 'x',
-  },
-};
-
-testCards(config);
-testFacebook(config);
-testMessages(config);
-testReceive(config);
+testFacebook();
