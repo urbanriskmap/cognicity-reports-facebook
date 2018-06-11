3e75ebf969050c69491ecd078c691b534de86dff
Tomas <tomasholderness@gmail.com>
2018-06-11 14:53:37 -0400

    handle null instance region code

--- src/config.js
+++ src/config.js
@@ -5,6 +5,8 @@ export default {
     CARDS_API: process.env.CARDS_API || 'https://data.riskmap.us/cards/',
     CARDS_API_KEY: process.env.CARDS_API_KEY,
     CARDS_URL: process.env.CARDS_URL || 'https://cards.riskmap.us/flood/',
+    DEFAULT_INSTANCE_REGION_CODE: process.env.DEFAULT_INSTANCE_REGION_CODE ||
+        'brw',
     DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE || 'id',
     FACEBOOK_ENDPOINT: process.env.FACEBOOK_ENDPOINT || 'https://graph.facebook.com/v2.6/me/messages',
     FACEBOOK_PAGE_ACCESS_TOKEN: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
