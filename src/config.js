169031b1e24773302e5a1f5785149065823f6b4e
Tomas <tomasholderness@gmail.com>
2018-06-29 12:05:51 -0400

    add placeholder for app token for testing

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 require('dotenv').config({silent: true});
 
 export default {
@@ -5,12 +6,11 @@ export default {
     CARDS_API: process.env.CARDS_API || 'https://data.riskmap.us/cards/',
     CARDS_API_KEY: process.env.CARDS_API_KEY,
     CARDS_URL: process.env.CARDS_URL || 'https://cards.riskmap.us/flood/',
-    DEFAULT_INSTANCE_REGION_CODE: process.env.DEFAULT_INSTANCE_REGION_CODE ||
-        'brw',
+    DEFAULT_INSTANCE_REGION_CODE: process.env.DEFAULT_INSTANCE_REGION_CODE || 'brw',
     DEFAULT_LANGUAGE: process.env.DEFAULT_LANGUAGE || 'id',
     FACEBOOK_APP_SECRET: process.env.FACEBOOK_APP_SECRET,
     FACEBOOK_ENDPOINT: process.env.FACEBOOK_ENDPOINT || 'https://graph.facebook.com/v2.6/me/messages',
-    FACEBOOK_PAGE_ACCESS_TOKEN: process.env.FACEBOOK_PAGE_ACCESS_TOKEN,
+    FACEBOOK_PAGE_ACCESS_TOKEN: process.env.FACEBOOK_PAGE_ACCESS_TOKEN || 'token',
     FACEBOOK_VALIDATION_TOKEN: process.env.FACEBOOK_VALIDAION_TOKEN,
     MAP_URL: process.env.MAP_URL || 'https://dev.riskmap.us/',
     NETWORK_NAME: 'facebook',
