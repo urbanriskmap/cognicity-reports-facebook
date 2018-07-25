1c356cc33eab0f4c0004e6db35fa1b11d72dd4cf
Tomas <tomasholderness@gmail.com>
2018-07-25 17:10:13 -0400

    further logging

--- src/lib/facebook.js
+++ src/lib/facebook.js
@@ -130,7 +130,7 @@ export default class Facebook {
     return new Promise((resolve, reject) => {
       console.log('Sending request to facebook.');
       console.log(properties.request);
-      console.log(properties.body);
+      console.log(JSON.stringify(properties.body));
       this.axios.post(properties.request, properties.body)
         .then((response) => resolve(response))
         .catch((err) => reject(err));
